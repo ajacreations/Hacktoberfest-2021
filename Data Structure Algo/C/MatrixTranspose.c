2b7d5710a2586f4a305a0401b07534a73806e6d6
shuddhi-jain <92723741+shuddhi-jain@users.noreply.github.com>
2021-10-20 19:01:14 +0530

    Update MatrixTranspose.c

--- Data Structure Algo/C/MatrixTranspose.c
+++ Data Structure Algo/C/MatrixTranspose.c
@@ -1,42 +1,35 @@
-//To find out transpose of a matrix.
-
-
 #include<stdio.h>
-#include<stdlib.h>
 int main()
 {
-    int a[10][10], m,n,i,j,b[10][10];
+	int r,c,p,q,i,j;
+	printf("enter row and column for first matrix");
+	scanf("%d%d",&r,&c);
+	int a[10][10],b[10][10];
+	
+	printf("enter number for first matrix");
+		for(i=0;i<r;i++)
+		{
+			for(j=0;j<c;j++)
+			{
+			//printf("enter number for first matrix");
+			scanf("%d",&a[i][j]);
+		}}
+		printf("origional matrix\n");
+			for(i=0;i<r;i++)
+		{
+			for(j=0;j<c;j++)
+			{
+				b[j][i]=a[i][j];
+			printf("%d ",a[i][j]);}
+			printf("\n");
+		}
+		printf("transpose matrix\n");
+			for(i=0;i<c;i++)
+		{
+			for(j=0;j<r;j++)
+			printf("%d ",b[i][j]);
+			printf("\n");
+		}
+	}
 
-    printf("enter values for m and n:");
-    scanf("\t%d\t%d",&m,&n);
-
-    printf("\nEnter matrix elements\n");
-    for(i=0;i<m;i++)
-    {
-        for(j=0;j<n;j++)
-        {
-            scanf("\t%d",&a[i][j]);
-        }
-    }
-
-     for(i=0;i<m;i++)
-    {
-        for(j=0;j<n;j++)
-        {
-            b[j][i]=a[i][j];
-        }
-    }
-
-    printf("transpose of matrix is:\n");
-     for(i=0;i<n;i++)
-    {
-        for(j=0;j<m;j++)
-        {
-           printf("\t%d",b[i][j]);
-        }
-          printf("\n");
-    }
-
-    return 0;
-}
-
+	
